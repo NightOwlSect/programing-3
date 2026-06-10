cb06186f703105eed0750ab7cec480483ace7737
NightOwlSect <doyouroshi@gmail.com>
2026-06-10 16:35:28 +0000

    yes

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,12 @@
+import random
 
+#player_guess = 0
+
+secret_number = random,randrange(1,10)
+
+player_guess=int(input("Guess the secret number:"))
+
+if player_guess == secret_number:
+    print("You were right")
+
+print(secret_number)
